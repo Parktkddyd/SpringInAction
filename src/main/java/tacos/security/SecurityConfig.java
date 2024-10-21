9af7b676e79fb172964e2493fc3b0a459ba83e12
Parktkddyd <s_in_b_u@naver.com>
2024-10-21 22:19:38 +0900

    feat : SpringSecurity를 사용하여 보안 구성 - JDBC 기반 사용자 스토어를 통한 임시 로그인 및 접근 권한 테스트

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -11,7 +11,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 /*책의 실습에는 Spring Security 버전이  낮기 때문에 해당 클래스는 WebSecurityConfigurerAdapter 클래스를 상속받아 구현하였지만,
       현재 Spring Security 6.3.3 기준 해당 클래스는 지원 중단 되었고 SecurityFilterChain을 Bean으로 등록하여 사용함
@@ -20,12 +25,37 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig{
+    private final DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
+    //Jdbc
     @Bean
+    public UserDetailsService userDetailsService(){
+        JdbcUserDetailsManager jdbcUserDetailManager = new JdbcUserDetailsManager(dataSource);
+
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("password1"))
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password(passwordEncoder().encode("password2"))
+                .roles("USER")
+                .build();
+
+        jdbcUserDetailManager.createUser(user1);
+        jdbcUserDetailManager.createUser(user2);
+
+        return jdbcUserDetailManager;
+    }
+    // ImMemory
+    /*@Bean
     public UserDetailsService userDetailsService(){
         UserDetails user1 = User
                 .withUsername("user1")
@@ -38,17 +68,17 @@ public class SecurityConfig{
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user1, user2);
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-            http
+        http
                 .authorizeHttpRequests((request) ->
-                        request.requestMatchers("/design", "orders").hasRole("USER")
+                        request.requestMatchers("/design", "/orders").hasRole("USER")
                                 .requestMatchers("/", "/**").permitAll()
-                ).httpBasic(Customizer.withDefaults());
-
-            return http.build();
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
     }
 
 
